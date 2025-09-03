95598a700bd578e3dd09273d99b8093951467666
tiffany.duneau <tiffany@incubiq.com>
2025-09-03 19:04:29 +0200

    refactor following parsing

--- tutorials/utils/ansatz.py
+++ tutorials/utils/ansatz.py
@@ -18,23 +18,7 @@ def person_ar(self, box):
 PersonFunctor = Functor(grammar, lambda self, o: o, person_ar)
 
 
-class DBox(Box):
-    """Daggerable box. Implement this at the diagram level so that the axioms
-    can all be implemented in the same place. Later we adapt the ansatz to
-    handle this specific case.
-    """
-    def __init__(self, *args, dagger: bool = False, **kwargs):
-        self.dagger = dagger
-        super().__init__(*args, **kwargs)
-
-    def __str__(self):
-        return f"{super().__str__()}{'.dg' if self.dagger else ''}"
-
-    def __repr__(self):
-        return f"{super().__repr__()}{'.dg' if self.dagger else ''}"
-
-
-class DaggerableSim4Ansatz(Sim4Ansatz):
+class DiscardSim4Ansatz(Sim4Ansatz):
     def _ar(self, _: Functor, box: Box) -> Circuit:
         # Wrap the default implementation to automatically convert
         # custom discard and daggered boxes.
@@ -42,7 +26,5 @@ class DaggerableSim4Ansatz(Sim4Ansatz):
             return Discard()
 
         circuit = super()._ar(_, box)
-        if isinstance(box, DBox) and box.dagger:
-            circuit = circuit.dagger()
         return circuit
 
